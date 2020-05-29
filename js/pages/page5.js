dd620d45396b0479c1504a849acc8fc6548d2290
nidhijainsng <61142520+nidhijainsng@users.noreply.github.com>
2020-05-29 16:26:24 +0800

    Update page5.js

--- js/pages/page5.js
+++ js/pages/page5.js
@@ -43,7 +43,7 @@ $('input[type=checkbox]').each(function () {
 	   selectedList =selectedList.substr(0,selectedList.length-1);
 	 }
 	 page5ObjInner.setSelectedInventoryItems(selectedList);
-	 
+	 page5ObjInner.setTotalInvExp(document.getElementById("totalInvExpTxt").value);
 	alert(selectedList);
 }
 
@@ -61,8 +61,46 @@ bakery.restoreInventoryDataObject = function(){
 		  document.getElementById(selectedChkBox[i]).checked = true; 
 		}	
 	}
+	alert(page5ObjInner.getTotalInvExp());
+	alert(document.getElementById("totalInvExpTxt").value);
+	document.getElementById("totalInvExpTxt").value=page5ObjInner.getTotalInvExp();
+	
  }
  
+}
+
+bakery.evaluateInvExp= function(){
+var enteredVal=document.getElementById("totalInvExpTxt").value;
+var cntCheckedBox=1;
+var actualTotalVal=0;
+$('input[type=checkbox]').each(function () {
+
+	if(this.checked)
+	{
+	var selectedItmPrice=chkBoxImageAndPrice[cntCheckedBox-1][1];
+	  actualTotalVal=parseInt(actualTotalVal)+parseInt(selectedItmPrice.substring(1,selectedItmPrice.length));	  
+	  cntCheckedBox++;	  
+	}
+	else
+	{
+	  cntCheckedBox++;
+	}
+	 
+	});	
+	if(actualTotalVal==enteredVal)
+	{
+	   document.getElementById("nxtButtonId").style.display="inline";
+	   document.getElementById('evalResult').innerHTML="<img src='images/confirm.svg'/>&nbsp;Correct";
+	   document.getElementById("evalResult").classList.remove('fixedEvalWrong');
+	   document.getElementById("evalResult").classList.add('fixedEvalCorrect');
+	}
+	else
+	{
+	  document.getElementById('evalResult').innerHTML="Oops, try again";
+	  document.getElementById("evalResult").classList.remove('fixedEvalCorrect');
+      document.getElementById("evalResult").classList.add('fixedEvalWrong');
+	  document.getElementById("nxtButtonId").style.display="none";
+	}
 }
 
 var chkBoxImageAndPrice = [
