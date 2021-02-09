578de6870e153b442d041883cf008769bf051204
nidhijainsng <61142520+nidhijainsng@users.noreply.github.com>
2021-02-09 15:30:25 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -436,44 +436,7 @@ if(retVal)
 			document.getElementById("showCalculator").innerHTML=bakeryShowCalc(bakery.pages);
 			
 			bakery.checkResultExpValueBrkEven();
-			
-			 // document.getElementById('bakeryNameLblDynamic').innerHTML=pageArray[0].getShopName();
-			 var scrollingImages="";
-			 scrollingImages=scrollingImages+'<li><img src="'+'images/cafe.svg '+'" style="height:100px" /></li>'+
-			'<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>';
-			
-			if(pageArray[1]!=undefined && pageArray[1].getLocationImgInx() !=undefined && factsDetails !=undefined)
-			{
-					scrollingImages=scrollingImages+'<li><img src="'+factsDetails[pageArray[1].getLocationImgInx()][0]+'" style="height:100px" /></li>'+
-					'<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>';
-					
-					
-					var emp1Qty=pageArray[2].getEmpQty1();
-					for(var cnter=0;cnter<emp1Qty;cnter++)
-					{
-					 scrollingImages= scrollingImages+'<li><img src="'+staff[0][3]+'" style="height:100px" /></li>'+
-					'<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>';
-					}
-					
-					var srcVal=""; 
-					var empImgString="";
-					var selectedChkBox=pageArray[4].getSelectedInventoryItems().split(",");
-			
-						 if(selectedChkBox != '')
-						 {
-							for (i = 0; i < selectedChkBox.length; i++) 
-							{						
-							  srcVal=chkBoxImageAndPrice[parseInt(selectedChkBox[i].substr(2,3))-1][0];
-							  scrollingImages =scrollingImages+'<li><img src="'+srcVal+'" style="height:100px" /></li>'+
-							  '<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>';
-							  
-							}	
-						}
-					
-					 document.getElementById('populatescrollingImagesIndex').innerHTML=scrollingImages;
-					
-					 bakery.scrollTheImages();
-			}
+	
 			 bakery.underlineTextOnFocus(document.getElementById("brkEvenAnchorId"));
 			 
 			 
